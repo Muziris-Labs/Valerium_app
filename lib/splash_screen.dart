e4829f25ec5eaff4e6986851e3c5c811a269ff83
Arpitha <arpithasudhir18@gmail.com>
2024-04-05 01:27:38 +0530

    on-board

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:design_grid/design_grid.dart';
-import 'main.dart';
+import 'onboarding.dart'; // Import your onboarding page file here
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Navigates to the onboarding page after 2 seconds
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => OnboardingPage()),
+      );
+    });
+
     return Scaffold(
       backgroundColor: Color.fromARGB(1, 33, 32, 32),
       body: Stack(
         children: [
-          // Grid paper background
-          GridPaper(
-            color: Color.fromARGB(115, 100, 96, 96), // Adjust grid color as needed
-            interval: 110, // Adjust grid interval as needed
-            subdivisions: 1, // Set subdivisions to 0 to remove subdivisions
-            child: Container(), // Empty container as we only need the grid
-          ),
+          
+          
           // Centered image
           Center(
             child: Image.asset(
               'lib/images/valerium2.png',
-              width: 200, // Set the width of the image
-              height: 200, // Set the height of the image
+              width: 200, 
+              height: 200, 
             ),
           ),
         ],
